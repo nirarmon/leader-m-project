b3f05642774447921b2dbf21a41c9e12a5a621d1
Michael Oziransky <oziransky@gmail.com>
2014-09-08 08:34:19 +0300

    Merge branch 'master' of https://github.com/nirarmon/leader-m-project
    Conflicts:
    	src/main/resources/scenarios/AffiliateReport.properties
    	src/main/resources/scenarios/Stats.properties

--- src/main/java/com/leaderm/infra/StatsReport.java
+++ src/main/java/com/leaderm/infra/StatsReport.java
@@ -34,7 +34,7 @@ public class StatsReport extends AbstractPage {
 		WebElement password = driver.findElement(By.name("password"));
 		WebElement ok = driver.findElement(By.xpath("//button[1]"));
 		username.sendKeys("lm");
-		password.sendKeys("EwmswjLgHNv72Tu");
+		password.sendKeys("be!Ot-FbQhBy");
 		ok.click();
 
 	}
