5f87bd0860cd419bd2d656e558a8c87a0c2cffb3
Michael Oziransky <oziransky@gmail.com>
2014-11-20 12:17:15 +0200

    pass update

--- src/main/java/com/leaderm/infra/StatsReport.java
+++ src/main/java/com/leaderm/infra/StatsReport.java
@@ -34,7 +34,7 @@ public class StatsReport extends AbstractPage {
 		WebElement password = driver.findElement(By.name("password"));
 		WebElement ok = driver.findElement(By.xpath("//button[1]"));
 		username.sendKeys("lm");
-		password.sendKeys("V^d5Xv^6ct%!");
+		password.sendKeys("CIT_R&*g-KyN");
 		ok.click();
 
 	}
